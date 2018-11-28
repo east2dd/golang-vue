baaa480669ab11eb70146f9e337bccc455304db0
alex <xingxing2d@gmail.com>
2018-11-28 23:34:04 +1000

    [client] products layout in single category page

--- client/src/components/product/ProductItem.vue
+++ client/src/components/product/ProductItem.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <router-link
-            tag="p"
-            :to="'/products/' + this.item.ID"
-            style="cursor: pointer"><a href="javascript:void(0)">{{ this.item.Name }}</a></router-link>
+    <div class="product">
+        <div class="row">
+            <div class="columns medium-4">
+                <router-link
+                    tag="p"
+                    :to="'/products/' + this.item.ID"
+                    style="cursor: pointer"><a href="javascript:void(0)">{{ this.item.Name }}</a></router-link>
+            </div>
 
-        <p>Price: {{ this.item.Price }}</p>
-        <p>{{ this.item.Description }}</p>
+            <div class="columns medium-8">
+                <p>Price: {{ this.item.Price }}</p>
+                <p>{{ this.item.Description }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,3 +21,9 @@ export default {
     props: ['item'],
 }
 </script>
+
+<style scoped>
+  .product {
+      margin-bottom: 2em;
+  }
+</style>
