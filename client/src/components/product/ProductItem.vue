cdc82bda05fc5660d1eef5d4ba3e4078b3edffb4
alex <xingxing2d@gmail.com>
2018-11-28 23:35:10 +1000

    [client] added product image placeholder

--- client/src/components/product/ProductItem.vue
+++ client/src/components/product/ProductItem.vue
@@ -2,13 +2,14 @@
     <div class="product">
         <div class="row">
             <div class="columns medium-4">
+                <p><img src="https://via.placeholder.com/300.png" style="width: 300px;"/></p>
+            </div>
+
+            <div class="columns medium-8">
                 <router-link
                     tag="p"
                     :to="'/products/' + this.item.ID"
                     style="cursor: pointer"><a href="javascript:void(0)">{{ this.item.Name }}</a></router-link>
-            </div>
-
-            <div class="columns medium-8">
                 <p>Price: {{ this.item.Price }}</p>
                 <p>{{ this.item.Description }}</p>
             </div>
