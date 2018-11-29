d53c4e4cff84552359869380a31632e615866e45
alex <xingxing2d@gmail.com>
2018-11-30 00:01:31 +1000

    [api] removed empty lines in auth controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -22,7 +22,6 @@ var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var Authenticate = func(w http.ResponseWriter, r *http.Request) {
-
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account)
 	if err != nil {
